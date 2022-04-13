b58277f1aca4a4e6dedc1290f945d20c05bc66e4
kule1163 <batuhankir1163@gmail.com>
2022-04-13 15:40:59 +0300

    movie-app-list

--- src/components/homeComp/movies/noMatched/NoMatchedSearch.tsx
+++ src/components/homeComp/movies/noMatched/NoMatchedSearch.tsx
@@ -2,15 +2,25 @@ import React from 'react'
 import { useAppSelector } from '../../../../app/hooks'
 import { MainContainer, TextBox, Text } from './styles'
 import { blue } from '@mui/material/colors'
+import { useNavigate } from 'react-router-dom'
 
 const NoMatchedSearch = () => {
     
     const searchValue = useAppSelector(state => state.movie.searchValue)
+
+    const navigate = useNavigate()
   
     return (
     <MainContainer>
         <TextBox>
-            <Text>No Matched By "{searchValue}" <b style={{cursor: "pointer", color: `${blue[400]}`}}>Go Home</b></Text>
+            <Text>No Matched By "{searchValue}" 
+            <b 
+                onClick={() => navigate("/")} 
+                style={{cursor: "pointer", color: `${blue[400]}`}}
+            >
+                Go Home
+            </b>
+            </Text>
         </TextBox>
     </MainContainer>
   )
